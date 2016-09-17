c4e826f319d09b0f9d9fbfe7a128e44fb350116d
codeluggage <matias.forbord@gmail.com>
2016-09-17 11:29:00 -0400

    latest changes before continuing

--- ShortcutWizard/index.macos.js
+++ ShortcutWizard/index.macos.js
@@ -12,11 +12,37 @@ const {
     Image,
     View,
     TouchableOpacity,
+    RecyclerViewBackedScrollView,
+    TouchableHighlight,
+    ListView,
 } = ReactNative;
 
 
+var LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ius ad pertinax oportere accommodare, an vix civibus corrumpit referrentur. Te nam case ludus inciderint, te mea facilisi adipiscing. Sea id integre luptatum. In tota sale consequuntur nec. Erat ocurreret mei ei. Eu paulo sapientem vulputate est, vel an accusam intellegam interesset. Nam eu stet pericula reprimique, ea vim illud modus, putant invidunt reprehendunt ne qui.';
+
+/* eslint no-bitwise: 0 */
+var hashCode = function(str) {
+  var hash = 15;
+  for (var ii = str.length - 1; ii >= 0; ii--) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(ii);
+  }
+  return hash;
+};
 
 const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#F6F6F6',
+  },
+  thumb: {
+    width: 64,
+    height: 64,
+  },
+  text: {
+    flex: 1,
+  },
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -33,10 +59,6 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
-  row: {
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
   icon: {
     width: 24,
     height: 24,
@@ -137,10 +159,96 @@ const ShortcutWizard = React.createClass({
         console.log('>>> componentWillUnmount()');
         console.log((this.props) ? this.props : "No props");
     },
-    render() {
-        this.state = {
-            image: require(this.props.applicationIconPath)
-        };
+  statics: {
+    title: '<ListView>',
+    description: 'Performant, scrollable list of data.'
+  },
+
+  getInitialState: function() {
+    console.log('>>>>>>>>>> hit initial state');
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows(this._genRows({})),
+    };
+  },
+
+  _pressData: ({}: {[key: number]: boolean}),
+
+  componentWillMount: function() {
+    this._pressData = {};
+  },
+
+  render: function() {
+
+    this.state = {
+        image: this.props.applicationIconPath ? this.props.applicationIconPath : ''
+    };
+
+    return (
+        <View style={{flexDirection: 'row'}}>
+            <Image style={{
+                width: 30,
+                height: 30,
+                backgroundColor: 'transparent',
+                marginRight: 10,
+            }} source={{uri: this.props.applicationIconPath}} /> 
+
+            <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this._renderRow}
+            renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+            renderSeparator={this._renderSeparator}
+            />
+        </View>
+        );
+  },
+
+  _renderRow: function(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
+    var rowHash = Math.abs(hashCode(rowData));
+    return (
+      <TouchableHighlight onPress={() => {
+          this._pressRow(rowID);
+          highlightRow(sectionID, rowID);
+        }}>
+        <View>
+          <View style={styles.row}>
+            <Text style={styles.text}>
+              {rowData + ' - ' + LOREM_IPSUM.substr(0, rowHash % 301 + 10)}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  },
+
+  _genRows: function(pressData: {[key: number]: boolean}): Array<string> {
+    var dataBlob = [];
+    for (var ii = 0; ii < 100; ii++) {
+      var pressedText = pressData[ii] ? ' (pressed)' : '';
+      dataBlob.push('Row ' + ii + pressedText);
+    }
+    return dataBlob;
+  },
+
+  _pressRow: function(rowID: number) {
+    this._pressData[rowID] = !this._pressData[rowID];
+    this.setState({dataSource: this.state.dataSource.cloneWithRows(
+      this._genRows(this._pressData)
+    )});
+  },
+
+  _renderSeparator: function(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+    return (
+      <View
+        key={`${sectionID}-${rowID}`}
+        style={{
+          height: adjacentRowHighlighted ? 4 : 1,
+          backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+        }}
+      />
+    );
+  }
+    // render() {
                     // {this.props.icon.map(this.renderImage)}
         // <TouchableOpacity
         //     style={styles.wrapper}
@@ -171,18 +279,18 @@ const ShortcutWizard = React.createClass({
         //     </Text>
         //   </TouchableOpacity>
 
-        return (
-            <View style={{flexDirection: 'row'}}>
+        // return (
+        //     <View style={{flexDirection: 'row'}}>
 
-                <Image style={{
-                    width: 30,
-                    height: 30,
-                    backgroundColor: 'transparent',
-                    marginRight: 10,
-                }} source={{uri: this.props.applicationIconPath}} />
-            </View>
-            );
-    }
+        //         <Image style={{
+        //             width: 30,
+        //             height: 30,
+        //             backgroundColor: 'transparent',
+        //             marginRight: 10,
+        //         }} source={{uri: this.props.applicationIconPath}} />
+        //     </View>
+        //     );
+    // }
 });
 
             // <View style={styles.container}>
