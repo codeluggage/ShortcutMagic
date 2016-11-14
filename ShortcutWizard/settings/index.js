4ad0de72c1e2f5da5359e7a6e3ccfa9c426cac90
codeluggage <matias.forbord@gmail.com>
2016-11-14 09:11:06 -0500

    Fixed issue with settings include path

--- ShortcutWizard/settings/index.js
+++ ShortcutWizard/settings/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Settings from 'Settings.jsx';
+import Settings from './Settings.jsx';
 
 window.onload = function(){
 	ReactDOM.render(<Settings />, document.getElementById("settings-root"));
