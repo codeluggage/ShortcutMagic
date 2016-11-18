67d1b6b2ef7b9e48fbcda3138e1e694224d35ca5
codeluggage <matias.forbord@gmail.com>
2016-11-17 19:00:34 -0500

    settings editable within settings window

--- ShortcutWizard/settings/Settings.jsx
+++ ShortcutWizard/settings/Settings.jsx
@@ -22,23 +22,52 @@ export default class Settings extends Component {
     }
 
     handleChangeComplete(color) {
+    	console.log('hit handleChangeComplete with color ', color);
     	this.setState({ background: color.hex });
     }
 
     render() {
     	if (this.state) {
+    		console.log('about to render settings with state: ', this.state);
     		return (
-    			<div>
-	    			<ul>
-			        	<li>Transparency: {(this.state.alpha) ? "true" : "false"}</li>
-			        	<li>Float on top: {(this.state.alwaysOnTop) ? "true" : "false"}</li>
-			        	<li>Hide individually per app: {(this.state.hidePerApp) ? "true" : "false"}</li>
-			        	<li>Size and position individually per app: {(this.state.boundsPerApp) ? "true" : "false"}</li>
-	    			</ul>
-	    			<PhotoshopPicker 
-	    				color={this.state.background}
-		    			onChangeComplete={this.handleChangeComplete}
-	    			/>
+    			<div style={{backgroundColor: this.state.background}}>
+			        	<li>
+		                    <button id="reload-button" className="simple-button" onClick={() => {
+		                    	this.setState({alpha: !this.state.alpha});
+		                    }}>
+			                    Transparency: {(this.state.alpha) ? "true" : "false"}
+		                    </button>
+			        	</li>
+			        	<li>
+		                    <button id="reload-button" className="simple-button" onClick={() => {
+		                    	this.setState({alwaysOnTop: !this.state.alwaysOnTop});
+		                    }}>
+			                    Float on top: {(this.state.alwaysOnTop) ? "true" : "false"}
+		                    </button>
+			        	</li>
+
+			        	<li>
+		                    <button id="reload-button" className="simple-button" onClick={() => {
+		                    	this.setState({hidePerApp: !this.state.hidePerApp});
+		                    }}>
+					        	Hide individually per app: {(this.state.hidePerApp) ? "true" : "false"}
+		                    </button>
+			        	</li>
+
+			        	<li>
+		                    <button id="reload-button" className="simple-button" onClick={() => {
+		                    	this.setState({boundsPerApp: !this.state.boundsPerApp});
+		                    }}>
+					        	Size and position individually per app: {(this.state.boundsPerApp) ? "true" : "false"}
+		                    </button>
+			        	</li>
+			        	<li>
+			        		Choose color: 
+			    			<PhotoshopPicker 
+			    				color={this.state.background}
+				    			onChangeComplete={this.handleChangeComplete}
+			    			/>
+			    		</li>
 	    		</div>
 			);
 	  //   	return (
@@ -51,7 +80,7 @@ export default class Settings extends Component {
 	  //   		</ul>
 			// );
 	    } else {
-	    	return (<h1>hello from settings!</h1>);
+	    	return (<h1>Settings loading...</h1>);
 	    }
     }
 }
