211cc4d22790e8ecabd2018fe8b08d8a88ca55b3
codeluggage <matias.forbord@gmail.com>
2017-06-23 19:30:21 -0400

    Improved default mac style

--- ShortcutMagic/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './components/Home.jsx';
-
-window.onload = function(){
-  ReactDOM.render(<Home />, document.getElementById("app"));
-}
