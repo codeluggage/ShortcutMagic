1c8fc63cc77b940c71859c3a76e922829f9ea880
codeluggage <matias.forbord@gmail.com>
2017-11-27 08:36:12 -0500

    Fix app signing paths

--- ShortcutMagic/sign_app.sh
+++ ShortcutMagic/sign_app.sh
@@ -8,7 +8,8 @@
 # 2. Now set the value of the identity variable below to the identity you want to use:
 identity="Developer ID Application: Matias Forbord (A9ADPQA768)"
 
-app="$PWD/$1"
+appPath="$PWD/$1"
+appName=$2
 
 echo "Signing..."
 # When you sign frameworks, you have to sign a specific version.
@@ -19,23 +20,23 @@ echo "Signing..."
 # Other scripts sometimes resign items multiple times in the process because of --deep which is slow.
 # The following signs the bare minimum needed to get Gatekeeper acceptance.
 # If you renamed "Electron Helper.app",  "Electron Helper EH.app" and "Electron Helper NP.app" then rename below.
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/Electron Framework.framework/Versions/A"
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/Electron Helper EH.app"
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/Electron Helper NP.app"
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/Electron Helper.app"
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/Mantle.framework/Versions/A"
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/ReactiveCocoa.framework/Versions/A"
-codesign --verbose --force --sign "$identity" "$app/Contents/Frameworks/Squirrel.framework/Versions/A"
-codesign --verbose --force --sign "$identity" "$app"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/Electron Framework.framework/Versions/A"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/$appName Helper EH.app"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/$appName Helper NP.app"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/$appName Helper.app"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/Mantle.framework/Versions/A"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/ReactiveCocoa.framework/Versions/A"
+codesign --verbose --force --sign "$identity" "$appPath/Contents/Frameworks/Squirrel.framework/Versions/A"
+codesign --verbose --force --sign "$identity" "$appPath"
 
 # This will often pass, even if Gatekeeper fails.
 echo ""
 echo "Verifying signatures..."
-codesign --verify --deep --display --verbose=4 "$app"
+codesign --verify --deep --display --verbose=4 "$appPath"
 
 # This is what really counts and what the user will see.
 echo ""
 echo "Veriyfing Gatekeeper acceptance..."
-spctl --ignore-cache --no-cache --assess --type execute --verbose=4 "$app"
+spctl --ignore-cache --no-cache --assess --type execute --verbose=4 "$appPath"
 
 # Thanks to http://jbavari.github.io/blog/2015/08/14/codesigning-electron-applications/
