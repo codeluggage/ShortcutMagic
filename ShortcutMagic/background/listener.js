7bbe4304dee85ace3840c13c52ebdd64fe64051c
codeluggage <matias.forbord@gmail.com>
2018-02-10 18:26:14 -0500

    Clear out gh-pages folder

--- ShortcutMagic/background/listener.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { ipcRenderer } = require('electron');
-const appSwitchListener = require('../shared/appSwitchListener');
-
-window.onload = function () {
-	appSwitchListener(function(appName) {
-		ipcRenderer.send('main-app-switched-notification', appName);
-	});
-};
--- ShortcutMagic/welcome/welcome.js
+++
@@ -1,4 +0,0 @@
-// TODO:
-// load anything from here?
-// attempt to do an assistive access operation that will cause it to open a confirmation window?
-// save show/hide welcome window checkbox settings
